77462883ea347fbfe87a9ef37247a8f6efc09f1d
Your Name <leonardo_foz@hotmail.com>
2023-02-20 16:16:37 -0300

    Criado atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,19 +1,22 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=35):
+    def __init__(self, *filhos, nome=None, idade=35):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
         
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
-
-
 if __name__ == '__main__':
-    p = Pessoa()
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Leo'
-    print(p.nome)
-    print(p.idade)
+    leo = Pessoa(nome = 'Leo')
+    carlos = Pessoa(leo, nome = 'Carlos')
+    print(Pessoa.cumprimentar(carlos))
+    print(id(carlos))
+    print(carlos.cumprimentar())
+    print(carlos.nome)
+    print(carlos.idade)
+    for filho in carlos.filhos:
+        print(f'filhos do for: {filho.nome}')
+
+
+
